fea2b95e55b95bbceecac464c06968ebd54128f3
aremumahmud <74737123+aremumahmud@users.noreply.github.com>
2022-01-03 21:47:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var router = require("./router")
 var app = express()
 app.use("/api",router)
 app.listen(process.env.PORT || 2000)
-var uri = `mongodb://FileuploadService:aremu2003@cluster0-shard-00-00.brdt8.mongodb.net:27017,cluster0-shard-00-01.brdt8.mongodb.net:27017,cluster0-shard-00-02.brdt8.mongodb.net:27017/totalcare?ssl=true&replicaSet=atlas-cbfjj2-shard-0&authSource=admin&retryWrites=true&w=majority`
+var uri = process.env.URI
 mongoose.connect(uri)
     .then(res=>{
       console.log(res ,"connected")
